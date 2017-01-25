90742bc8a442333fc4f764dcb4cb70f06ae63f60
saravanan kumar <sara14316.cs@rmkec.ac.in>
2017-01-25 22:55:54 +0530

    first commit

---
+++ 1or2Mat/1or2Mat/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  1or2Mat
+//
+//  Created by Bharadwaj on 24/01/16.
+//  Copyright © 2016 Bharadwaj. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+    
+    return 0;
+}
